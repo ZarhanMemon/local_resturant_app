a6f88d83f016a4602e15daad459a54ac79db8216
Zarhan Memon <ironmanred118@gmail.com>
2026-02-07 23:02:50 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 // Ensure the URL matches your Render URL exactly + /api
 const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:5000/api" 
-  : "https://vingo-backend-isei.onrender.com"; 
+  : "https://vingo-backend-isei.onrender.com"; // Verify this exactly
+
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
